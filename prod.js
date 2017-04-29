9632052acaf03abcc7b3595de85463d7bf4a8174
defehub <drazewski.przemyslaw@gmail.com>
2017-04-29 14:00:21 +0200

    Add prod.js

--- prod.js
+++ prod.js
@@ -1,6 +1,14 @@
 var express = require('express');
+var path = require('path');
 var app = express();
 
-app
-app.use(express.static('dist'))
-app.listen(8089);
+// Define the port to run on
+app.set('port', 8089);
+
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Listen for requests
+var server = app.listen(app.get('port'), function() {
+  var port = server.address().port;
+  console.log('Magic happens on port ' + port);
+});
