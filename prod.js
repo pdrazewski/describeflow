0ad03de3b44710ab775150dd2837d9e1b8286947
pdrazewski <przemyslaw@MacBook-Air-Karolina.local>
2017-04-29 13:30:54 +0200

    Reload app

--- prod.js
+++ prod.js
@@ -2,9 +2,5 @@ var express = require('express');
 var app = express();
 
 app
-.use("/dist", express.static(__dirname + '/dist'))
-.use(express.static('views'))
-.get('/', function (req, res) {
-   res.sendFile('dist/index.html');
-})
-.listen(8089);
+app.use(express.static('dist'))
+app.listen(8089);
